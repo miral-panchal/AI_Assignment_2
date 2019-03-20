f08103d3aea84a5b5f0e811224cfef238398570d
miral-panchal <miral.panchal@uoit.net>
2019-03-20 13:42:28 -0400

    Added City CSV File

--- GeneticAlgorithm_TSP/src/sample/Main.java
+++ GeneticAlgorithm_TSP/src/sample/Main.java
@@ -6,11 +6,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
+        try {
+            File file = new File("TEST_DATA.csv");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) { e.printStackTrace(); }
         //Initialize Population
         Population population = new Population();
 
